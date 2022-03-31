27c56fef77dbd4dc7c0c6e1b84507c226f6136a4
stivenwar <estebanguerra1990@gmail.com>
2022-03-31 16:33:01 +0200

    front-end web employees angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListaEmpleadosComponent } from './lista-empleados/lista-empleados.component';
+import { RegisterEmpleadoComponent } from './register-empleado/register-empleado.component';
+import {ActualizarEmpleadoComponent} from "./actualizar-empleado/actualizar-empleado.component";
+import {EmpleadoDetallesComponent} from "./empleado-detalles/empleado-detalles.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'empleados',component:ListaEmpleadosComponent},
+  {path : '', redirectTo:'empleados',pathMatch:'full'},
+  {path : 'registrar-empleado', component: RegisterEmpleadoComponent},
+  {path : 'actualizar-empleado/:id', component: ActualizarEmpleadoComponent},
+  {path : 'empleado-detalles/:id', component: EmpleadoDetallesComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ListaEmpleadosComponent } from './lista-empleados/lista-empleados.component';
+import { HttpClientModule} from '@angular/common/http';
+import { RegisterEmpleadoComponent } from './register-empleado/register-empleado.component'
+import { FormsModule } from '@angular/forms';
+import { ActualizarEmpleadoComponent } from './actualizar-empleado/actualizar-empleado.component';
+import { EmpleadoDetallesComponent } from './empleado-detalles/empleado-detalles.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaEmpleadosComponent,
+    RegisterEmpleadoComponent,
+    ActualizarEmpleadoComponent,
+    EmpleadoDetallesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
